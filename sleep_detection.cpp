a98919d7e345f8186ff1d9d84595b577541cc56e
GyeongHo Kim <42205408+GyeongHoKim@users.noreply.github.com>
2020-10-29 00:12:45 +0900

    눈꺼풀 감김 정도 확인 기능 구현

---
+++ sleep_detection.cpp
@@ -0,0 +1,125 @@
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_io.h>
+#include <unistd.h>
+
+using namespace dlib;
+using namespace std;
+using namespace cv;
+
+image_window win;
+shape_predictor sp;
+std::vector<cv::Point> righteye;
+std::vector<cv::Point> lefteye;
+char c;
+cv::Point p;
+
+double compute_EAR(std::vector<cv::Point> vec)
+{
+
+    double a = cv::norm(cv::Mat(vec[1]), cv::Mat(vec[5]));
+    double b = cv::norm(cv::Mat(vec[2]), cv::Mat(vec[4]));
+    double c = cv::norm(cv::Mat(vec[0]), cv::Mat(vec[3]));
+    //compute EAR
+    double ear = (a + b) / (2.0 * c);
+    return ear;
+}
+
+int main()
+{
+    int frameCounter = 0;
+    float threshold = 0.2;
+    try {
+        cv::VideoCapture cap(0);
+
+        if (!cap.isOpened()) {
+            cerr << "Unable to connect to camera" << endl;
+            return 1;
+        }
+        cap.set(CAP_PROP_FRAME_WIDTH, 640); //use small resolution for fast processing
+        cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+        cap.set(CAP_PROP_FPS, 30); //fps to 30
+
+        // Load face detection and deserialize  face landmarks model.
+        frontal_face_detector detector = get_frontal_face_detector();
+
+        deserialize("../shape_predictor_68_face_landmarks.dat") >> sp;
+
+        // Grab and process frames until the main window is closed by the user.
+        while (!win.is_closed()) {
+            // Grab a frame
+            cv::Mat temp;
+            if (!cap.read(temp)) {
+                break;
+            }
+
+            cv_image<bgr_pixel> cimg(temp);
+            full_object_detection shape;
+            
+            // Detect faces
+            std::vector<rectangle> faces = detector(cimg);
+            //cout << "Number of faces detected: " << faces.size() << endl;
+
+            win.clear_overlay();
+            win.set_image(cimg);
+            
+            // Find the pose of each face.
+            if (faces.size() > 0) {
+
+                shape = sp(cimg, faces[0]); //work only with 1 face
+
+                for (int b = 36; b < 42; ++b) {
+                    p.x = shape.part(b).x();
+                    p.y = shape.part(b).y();
+                    lefteye.push_back(p);
+                }
+                for (int b = 42; b < 48; ++b) {
+                    p.x = shape.part(b).x();
+                    p.y = shape.part(b).y();
+                    righteye.push_back(p);
+                }
+                //Compute Eye aspect ration for eyes
+                double right_ear = compute_EAR(righteye);
+                double left_ear = compute_EAR(lefteye);
+                
+                //if the avarage eye aspect ratio of lef and right eye less than 0.2, the status is sleeping.
+                if ((right_ear + left_ear) / 2 < threshold) {
+                    frameCounter++;
+                    if (frameCounter >= 27) {
+                        cout << "SLEEP" << endl;
+                        win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "SLEEP"));
+                    }
+                    else {
+                        win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "drowsing"));
+                    }
+                }
+                else {
+                    win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "not drowsing"));
+                    frameCounter = 0;
+                }
+                righteye.clear();
+                lefteye.clear();
+
+                win.add_overlay(render_face_detections(shape));
+
+                c = (char)waitKey(30);
+                if (c == 27)
+                    break;
+            }
+        }
+    }
+    catch (serialization_error& e) {
+        cout << "Check the path to dlib's default face landmarking model file to run this example." << endl;
+        cout << "You can get it from the following URL: " << endl;
+        cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
+        cout << endl
+             << e.what() << endl;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+}
